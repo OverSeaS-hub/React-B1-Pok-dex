fdc493d20a9769d0356cce29c4fd50034de595c5
malachai <malachaimorrison@gmail.com>
2024-10-10 15:36:18 +0200

    added second pokemon to list

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,11 +1,26 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
 export default function PokemonCard() {
+  const pokemon = {
+    image: pokemonList[1].imgSrc,
+    name: pokemonList[1].name,
+  };
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="Bulbasaur"
-      />
-      <figcaption>Bulbasaur</figcaption>
+      {pokemon.image != null ? (
+        <img src={pokemon.image} alt="Bulbasaur" />
+      ) : (
+        <p>???</p>
+      )}
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
